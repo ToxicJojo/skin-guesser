8369646869234e5490e9bb9d67241180e1e53dd1
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2020-10-20 16:18:32 +0200

    Basic guess selection

---
+++ src/components/game/SkinSelect.vue
@@ -0,0 +1,29 @@
+<template lang='pug'>
+  select(@change='$emit("input", skinList[$event.target.selectedIndex])' :value='value.id')
+    option(v-for='skin in skinList' :value='skin.id') {{ skin.name }}
+</template>
+
+<script>
+export default {
+  name: 'SkinSelect',
+  props: {
+    value: {
+      type: Object,
+      required: true,
+    },
+    champion: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    skinList () {
+      return this.champion.skins
+    },
+  },
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
--- src/views/SkinGuesserGame.vue
+++ src/views/SkinGuesserGame.vue
@@ -3,22 +3,31 @@
     div(v-if='isLoadingData')
       p Loading champion data
     template(v-else)
+      ChampionSelect(v-model='selectedChampion')
+      SkinSelect(v-model='selectedSkin' :champion='selectedChampion')
+      button(@click='checkGuess') Guess
       SkinDisplay(splashUrl='https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Talon_12.jpg' :clipData='clipData')
 </template>
 
 <script>
 import SkinDisplay from '@/components/game/SkinDisplay.vue'
+import ChampionSelect from '@/components/game/ChampionSelect.vue'
+import SkinSelect from '@/components/game/SkinSelect.vue'
 
 export default {
   name: 'SkinGuesserGamer',
   components: {
     SkinDisplay,
+    ChampionSelect,
+    SkinSelect,
   },
   data () {
     return {
       isLoadingData: false,
       currentChampion: undefined,
       currentSkin: undefined,
+      selectedChampion: {},
+      selectedSkin: {},
       clipData: {
         radius: 50,
         x: 50,
@@ -34,6 +43,13 @@ export default {
       this.isLoadingData = true
       await this.$store.dispatch('leagueData/loadChampions')
       this.isLoadingData = false
+    },
+    checkGuess () {
+      if (this.currentChampion.id === this.selectedChampion.id && this.currentSkin.id === this.selectedSkin.id) {
+        alert('Correct guess!')
+      } else {
+        alert(`Wrong it was: ${this.currentSkin.name}`)
+      }
     },
   },
 }
