a44fe660cf9188220546fb0d604c6001383cad83
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2020-10-20 12:23:49 +0200

    Changed templating language to pug

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
-<template>
-  <div id="app">
-    <router-view/>
-  </div>
+<template lang='pug'>
+  .app
+    h1 Skin Guesser
+    router-view
 </template>
 
-<style lang="scss">
+<style lang='scss'>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
