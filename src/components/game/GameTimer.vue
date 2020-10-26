a5b57aa1a95da936e7daa48535cf349695d1c498
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2020-10-26 11:52:09 +0100

    Added a basic time mode (#6)

---
+++ src/components/game/GameTimer.vue
@@ -0,0 +1,20 @@
+<template lang='pug'>
+  .game-timer
+    span {{ time }}
+</template>
+
+<script>
+export default {
+  name: 'GameTimer',
+  props: {
+    time: {
+      type: Number,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
