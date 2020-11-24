4b22f9110ceee18a4af24cd2d8b0c42732cff2c3
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2020-11-24 15:28:54 +0100

    Footer (#19)
    * Settings styling
    
    * Added a filter to tag group skins
    
    * Added an option to include group skins
    
    * Added a bunch of group skins to the filter list
    
    * Updated filter list
    
    * Added footer with required legal notice
    
    * Changed base url

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  footer
+    p SkinGuesser isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style lang="scss" scoped>
+
+footer {
+  margin: 32px;
+  font-size: 16px;
+  opacity: .5;
+  text-align: center;
+}
+
+</style>
